89f245ee83b990545dfa157ec14312f0df485497
reltoom <ariel.toom@gmail.com>
2024-03-08 11:49:55 +0100

    Very large Submit: created javascript so flipping works
    Matched css to classes so flipping boxes looks okay. created all javascript for flipping, matching, randomizing and starting game. Trial and error in VS until it worked.

---
+++ assets/javascript/index.js
@@ -0,0 +1,66 @@
+let smallBoxes = document.querySelectorAll('.small-box');
+let flippedBox = false;
+let firstBox, secondBox;
+let lockBox = false;
+
+window.onload = startGame();
+
+
+function startGame() {
+    smallBoxes.forEach((smallBox) => smallBox.addEventListener('click', flip));
+    shuffle();
+}
+
+
+
+//Flipping boxes 
+function flip() {
+    if (lockBox) return;
+    if (this === firstBox) return;
+    this.classList.add('flipped');
+
+    //first click
+    if (!flippedBox) {
+        flippedBox = true;
+        firstBox = this;
+        return;
+    }
+    //second click
+    flippedBox = false;
+    secondBox = this;
+    doesItMatch();
+}
+
+//Check to see if flipped boxes match
+function doesItMatch() {
+    let match = firstBox.title === secondBox.title;
+    if (match) {
+        disableBoxes();
+    } else {
+        flipBackBoxes();
+    }
+}
+
+//Stop clicking of visible boxes
+function disableBoxes() {
+    firstBox.removeEventListener('click', flip);
+    secondBox.removeEventListener('click', flip);
+}
+
+// Flip back boxes to face down
+function flipBackBoxes() {
+    lockBox = true;
+    setTimeout(() => {
+        firstBox.classList.remove('flipped');
+        secondBox.classList.remove('flipped');
+        lockBox = false;
+    }, 1500);
+}
+
+//Shuffle the location of the boxes 
+function shuffle() {
+    smallBoxes.forEach((box) => {
+        let randomize = Math.floor(Math.random() * 9);
+        box.style.order = randomize;
+    });
+}
