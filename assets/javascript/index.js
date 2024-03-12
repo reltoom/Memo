5b5e2b75f7a8946effa7904f765b26fa91bf57ae
reltoom <ariel.toom@gmail.com>
2024-03-12 11:17:39 +0100

    Deleted unused code

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -125,8 +125,3 @@ myWin.onclick = function () { close() };
 function close() {
     myWin.style.display = 'none';
 }
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = 'none';
-    }
-}
